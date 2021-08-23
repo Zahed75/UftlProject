23a298fc88c008438e6b141bb68c042a1de0ee92
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2021-08-23 08:22:44 +0600

    price func

--- static/js/price.js
+++ static/js/price.js
@@ -1,19 +1,32 @@
-
 document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
 var valueCount
 var fueltype = document.getElementById("fuelname").value;
 var delivery_charge = document.getElementById("delicharge").innerText;
 var octane_price = document.getElementById("opd").innerText;
 var diesel_price = document.getElementById("dpd").innerText;
+var discont = document.getElementById("discountSelect");
 // var price = 65
+var discontInput = document.getElementById("discountValue");
+
 
 function priceTotal() {
-    var total = valueCount * parseInt(diesel_price) + parseInt(delivery_charge);
+    var total = (valueCount * parseInt(diesel_price)) + parseInt(delivery_charge) - parseInt(discontInput.value);
     var base = valueCount * parseInt(diesel_price);
-    document.getElementById("price").innerText = total + '/-'
-    document.getElementById("fuel").innerText = base + '/-'
-    console.log(diesel_price)
+    document.getElementById("price").value = total
+    document.getElementById("fuel").value = base
+
 }
+
+
+discont.addEventListener('change', () => {
+    discontInput.value = parseInt(discont.options[discont.selectedIndex].value);
+
+    priceTotal()
+});
+
+var discountPrice = discontInput.value
+
+
 document.querySelector(".plus-btn").addEventListener("click", function() {
     valueCount = document.getElementById("quantity").value;
     valueCount++;
